65dd548456e3c1440e0e042dbe3191a1c5749e1c
anupm12 <shuklaanupam18@gmail.com>
2021-02-21 05:26:18 +0530

    Design complete

--- tryingGrip.py
+++ tryingGrip.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import * 
 import tkinter
 import pygame
 from PIL import ImageTk,Image 
@@ -12,25 +12,34 @@ class musicPlayer:
 
     def __init__(self, window):
         self.window = window
+        self.window.title("MusicPlayer")
+        self.window.configure(background='#4A4A4A')
         self.tracker=FALSE
         self.userInteface()
     
     def userInteface(self):
-        audioDetails = Frame(self.window)
+        self.playIcon = ImageTk.PhotoImage(Image.open("play-button.png"))
+        self.pauseIcon = ImageTk.PhotoImage(Image.open("pause.png"))
+        self.stopIcon = ImageTk.PhotoImage(Image.open("stop.png"))
+
+        audioDetails = Frame(self.window, bg="#4A4A4A", bd=10)
         audioDetails.grid(row=0, column=0, sticky="nsew")
 
-        self.canvas = Canvas(audioDetails)
-        self.img = ImageTk.PhotoImage(Image.open("haha.png"))
-        self.canvas.create_image(0, 0, image=self.img)
+        self.canvas = Canvas(audioDetails, bd=0, highlightthickness=0)
+        self.image = ImageTk.PhotoImage(Image.open("bg.jpg"))
+        self.canvas.create_image(0, 0, image=self.image)
         self.canvas.grid(row=0, column=0, sticky="nsew")
+
+        audioName = Label(audioDetails, text="Saturday nights", font=("Ubuntu Mono",30,""), bg="#4A4A4A", fg="white")
+        audioName.grid(row=0, column=0)
 
         audioDetails.grid_columnconfigure(0, weight=1)
         audioDetails.grid_rowconfigure(0, weight=1)
 
-        playlist = Frame(self.window)
+        playlist = Frame(self.window, bg="#4A4A4A", bd=10)
         playlist.grid(row=0, column=1, sticky="nsew")
 
-        self.playlistList = Listbox(playlist, selectmode=SINGLE, background="bisque")
+        self.playlistList = Listbox(playlist, selectmode=SINGLE, font=("Ubuntu Mono",11,""), bg="#4A4A4A", fg="white", bd=0, highlightthickness=0)
         self.playlistList.grid(row=0, column=0, sticky="nsew")
 
         os.chdir("/media/anupam/Local Disk1/Music/new/Music files/edm")
@@ -39,34 +48,22 @@ class musicPlayer:
             self.playlistList.insert(END,track)
 
         playlist.grid_columnconfigure(0, weight=1)
-        playlist.grid_rowconfigure(0, weight=1)
+        playlist.grid_rowconfigure(0, weight=1)        
 
         # pause button
-        self.pauseButton = Button(self.window, text="Pause", padx=40, pady=20)
+        self.pauseButton = Button(self.window, image=self.pauseIcon, bg="#4A4A4A", bd=0, highlightthickness=0)
         self.pauseButton.configure(command=self.pauseSong)
         self.pauseButton.grid(row=1, column=0)
 
         # stop button
-        stopButton = Button(self.window, text="Stop", padx=40, pady=20)
+        stopButton = Button(self.window, image=self.stopIcon, bg="#4A4A4A", bd=0, highlightthickness=0)
         stopButton.configure(command=self.stopSong)
         stopButton.grid(row=1, column=1)
 
-        self.window.grid_columnconfigure(0, weight=3)
+        self.window.grid_columnconfigure(0, weight=2)
         self.window.grid_columnconfigure(1, weight=1)
         self.window.grid_rowconfigure(0, weight=4)
         self.window.grid_rowconfigure(1, weight=1)
-
-        
-
-
-
-
-
-
-
-        
-
-
 
     def playSong(self):
         pygame.mixer.music.load("testSong.ogg")
