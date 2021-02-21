87830850272ae838392d1c6338787b1eecfa91c4
anupm12 <shuklaanupam18@gmail.com>
2021-02-21 05:57:40 +0530

    Added functionality

--- tryingGrip.py
+++ tryingGrip.py
@@ -30,8 +30,8 @@ class musicPlayer:
         self.canvas.create_image(0, 0, image=self.image)
         self.canvas.grid(row=0, column=0, sticky="nsew")
 
-        audioName = Label(audioDetails, text="Saturday nights", font=("Ubuntu Mono",30,""), bg="#4A4A4A", fg="white")
-        audioName.grid(row=0, column=0)
+        self.audioName = Label(audioDetails, text="Song Name", font=("Ubuntu Mono",30,""), bg="#4A4A4A", fg="white")
+        self.audioName.grid(row=0, column=0)
 
         audioDetails.grid_columnconfigure(0, weight=1)
         audioDetails.grid_rowconfigure(0, weight=1)
@@ -40,9 +40,11 @@ class musicPlayer:
         playlist.grid(row=0, column=1, sticky="nsew")
 
         self.playlistList = Listbox(playlist, selectmode=SINGLE, font=("Ubuntu Mono",11,""), bg="#4A4A4A", fg="white", bd=0, highlightthickness=0)
+        self.playlistList.bind('<Double-1>', self.playSong) 
         self.playlistList.grid(row=0, column=0, sticky="nsew")
 
-        os.chdir("/media/anupam/Local Disk1/Music/new/Music files/edm")
+        # os.chdir("/media/anupam/Local Disk1/Music/new/Music files/edm")
+        os.chdir("/media/anupam/Local Disk/Projects/Projects/music player")
         songtracks = os.listdir()
         for track in songtracks:
             self.playlistList.insert(END,track)
@@ -65,21 +67,21 @@ class musicPlayer:
         self.window.grid_rowconfigure(0, weight=4)
         self.window.grid_rowconfigure(1, weight=1)
 
-    def playSong(self):
-        pygame.mixer.music.load("testSong.ogg")
+    def playSong(self, event):
+        self.audioName.config(text=self.playlistList.get(ACTIVE))
+        pygame.mixer.music.load(self.playlistList.get(ACTIVE))
         pygame.mixer.music.set_volume(1.0)
         self.tracker=TRUE
-        self.pauseButton.configure(text="Pause")
         pygame.mixer.music.play()
     
     def pauseSong(self):
         if(self.tracker):
             pygame.mixer.music.pause()
-            self.pauseButton.configure(text="Play")
+            self.pauseButton.configure(image=self.playIcon)
             self.tracker=FALSE
         else:
             pygame.mixer.music.unpause()
-            self.pauseButton.configure(text="Pause")
+            self.pauseButton.configure(image=self.pauseIcon)
             self.tracker=TRUE
 
     def stopSong(self):
